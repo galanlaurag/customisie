c3852259a2d0e8c11025ed77284b93fb38519156
Laura Galan <galanlaurag@gmail.com>
2023-01-31 23:20:15 +0000

    navbar & styled components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 60px;
+    background-color: yellow;
+`
+const Navbar = () => {
+    return (
+        <Container>
+            This is navbar
+        </Container>
+
+    )
+}
+
+export default Navbar;
