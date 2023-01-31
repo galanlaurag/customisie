a010418e0efd771c12243aba71f129a635b65ff1
Laura Galan <galanlaurag@gmail.com>
2023-01-31 21:18:50 +0000

    new files

---
+++ src/components/homeComponents/newsletter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Newsletter = () => {
+    return (
+        <div className='subscribe-section bg-with-black'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-xs-12'>
+                        <div className='subscribe-head'>
+                            <h2>Do you want to subscribe out newsletter?</h2>
+                            <p>You can sign up here</p>
+                            <form className='form-section'>
+                                <input placeholder='Your email...' name='email' type='email' />
+                                <input value='Subscribe' name='subscribe' type='submit' />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Newsletter;
---
+++ src/components/homeComponents/shopSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ShopSection = () => {
+    return (
+        <div>
+            <h2>sndjk</h2>
+        </div>
+    )
+}
