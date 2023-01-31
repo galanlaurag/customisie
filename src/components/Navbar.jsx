7c96d2be3694b2f7fe5252732cbaa08f89448222
Laura Galan <galanlaurag@gmail.com>
2023-01-31 23:56:45 +0000

    syntax fix for styled components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,15 @@ import styled from 'styled-components';
 const Container = styled.div`
     height: 60px;
     background-color: yellow;
+    font-size: 2em;
 `
+
+
 const Navbar = () => {
     return (
         <Container>
             This is navbar
         </Container>
-
     )
 }
 
