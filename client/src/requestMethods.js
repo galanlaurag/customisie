5599bcf53f1a1a6c8b253eedfc72268e22895edc
Laura Galan <galanlaurag@gmail.com>
2023-03-16 20:33:34 +0000

    Deployed api using render & use deployed url instead of local one now

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -4,7 +4,8 @@ import axios from "axios";
 //     ? "http://localhost:80/api/"
 //     : "http://localhost:5000/api/";
 // const BASE_URL = "http://localhost:5000/api/";
-const BASE_URL = "http://localhost:80/api/";
+// const BASE_URL = "http://localhost:80/api/";
+const BASE_URL = "https://customisieapi.onrender.com/api/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGU4NzNmNzE3YjYwMGUwMzY4Njc0NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NTc4Njg1MiwiZXhwIjoxNjc2MDQ2MDUyfQ.J8mHiyQcFs_lPDCSlTaKs9EE1Be_jyg0PPjX9_e28VI";
 
 export const publicRequest = axios.create( {
